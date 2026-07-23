3cc88847451164bea94145f1e84c0ea357184a78
Ivanloe L. Manuel <imanuel6@ivytech.edu>
2026-07-23 18:30:09 -0400

    undoing member id changes

--- src/ui/member_view.py
+++ src/ui/member_view.py
@@ -145,7 +145,6 @@ class MemberView(tk.Frame):
         )
 
         fields = [
-            "Member ID",
             "First Name",
             "Last Name",
             "Phone",
@@ -210,7 +209,6 @@ class MemberView(tk.Frame):
 
     def add_member(self):
         try:
-            member_id = self.entries["Member ID"].get().strip()
             first_name = self.entries["First Name"].get().strip()
             last_name = self.entries["Last Name"].get().strip()
             phone = self.entries["Phone"].get().strip()
